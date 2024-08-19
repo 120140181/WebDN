0cdc0a44a9c5563bbde0a411544c20590b2f0499
Bima Setiawan Sandi <118245473+BimaSandi19@users.noreply.github.com>
2024-08-19 17:36:44 +0700

    form-submit

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
-use Illuminate\Auth\Events\Login;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LandingController;
-use App\Http\Controllers\DashboardController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 Route::resource('/', LandingController::class);
 Route::get('service', [LandingController::class, 'service'])->name('landing.service');
 Route::get('about', [LandingController::class, 'about'])->name('landing.about');
 Route::get('gallery', [LandingController::class, 'gallery'])->name('landing.gallery');
-Route::get('login', [DashboardController::class, 'index'])->name('dashboard.login');
-Route::post('login', [Dashboardcontroller::class, 'authenticate']);
+// auth routes
+Route::get('login', [AuthController::class, 'showLoginForm'])->name('auth.login');
+Route::post('login-proses', [AuthController::class, 'login_proses'])->name('login-proses');
+Route::post('logout', [AuthController::class, 'logout'])->name('logout-proses');
+// admin
 Route::get('haloadmin', [AdminController::class, 'index'])->name('admin.index');
 Route::get('reminder', [AdminController::class, 'reminder'])->name('admin.reminder');
 Route::get('history', [AdminController::class, 'history'])->name('admin.history');
