a90b1d6a514ae5faf890a0980924ce0ae77152dc
Bima Setiawan Sandi <118245473+BimaSandi19@users.noreply.github.com>
2024-09-28 18:42:36 +0700

    Add NoCache middleware and update Kernel configuration

---
+++ app/Http/Kernel.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    // Middleware lainnya
+
+    protected $routeMiddleware = [
+        // Middleware lainnya
+        'no-cache' => \App\Http\Middleware\NoCache::class,
+    ];
+}
